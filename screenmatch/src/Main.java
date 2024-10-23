7f12ce1e16489239ab10a59f2e2682448d3ab280
Emiliano R <ev.rdz22@outlook.com>
2024-10-22 18:23:59 -0600

    feat scoreAnalyzer y avverageScore

--- screenmatch/src/Main.java
+++ screenmatch/src/Main.java
@@ -6,15 +6,24 @@ public class Main {
         firstMovie.durationMinutes = 148;
 
         firstMovie.ShowSpecs();
+        firstMovie.scoreAnalyzer(9.3);
+        firstMovie.scoreAnalyzer(9.9);
+        firstMovie.scoreAnalyzer(6);
+        firstMovie.scoreAnalyzer(8);
+
+        System.out.println(firstMovie.averageScore());
+        System.out.println(firstMovie.scoreCount);
 
         Movie secondMovie = new Movie();
         secondMovie.name = "Figth Club";
         secondMovie.releaseDate = 1999;
         secondMovie.durationMinutes = 139;
 
+
         System.out.println("\nAqui la segunda pelicula");
 
          secondMovie.ShowSpecs();
+
 
     }
 }
