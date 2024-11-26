8a35c04d5bafa8848789386f65d5f5caa9a1e348
Emiliano R <ev.rdz22@outlook.com>
2024-11-26 12:31:59 -0600

    feat: Descripcion codigo

--- screenmatch/src/Main.java
+++ screenmatch/src/Main.java
@@ -1,18 +1,11 @@
 public class Main {
-    /** La clase main actúa como el punto de entrada del programa.
-      * Es donde se inicia la ejecución del código y se crean instancias de las clases definidas.
-      * Esto es esencial para organizar y estructurar el flujo del programa.
-      */
-
     public static void main(String[] args) {
-        // Instancia de la clase Movie
         Movie firstMovie = new Movie();
-        firstMovie.name = "Origen";
-        firstMovie.releaseDate = 2010;
-        firstMovie.durationMinutes = 148;
+        firstMovie.setName("Origen");
+        firstMovie.setReleaseDate(2010);
+        firstMovie.setDurationMinutes(148);
+        firstMovie.setIncludedInPlan(true);
 
-        // Utilizamos metodos descritos de la clase Movie
-        // .ShowSpecs y scoreAnalyzer
         firstMovie.ShowSpecs();
         firstMovie.scoreAnalyzer(9.3);
         firstMovie.scoreAnalyzer(9.9);
@@ -20,25 +13,21 @@ public class Main {
         firstMovie.scoreAnalyzer(8);
         firstMovie.scoreAnalyzer(5);
 
+        System.out.println(firstMovie.getScoreCount());
         System.out.println(firstMovie.averageScore());
-        System.out.println(firstMovie.scoreCount);
 
-        // Segunda instancia de la clase Movie
         Movie secondMovie = new Movie();
-        secondMovie.name = "Figth Club";
-        secondMovie.releaseDate = 1999;
-        secondMovie.durationMinutes = 139;
+        secondMovie.setName("Fight Club");
+        secondMovie.setReleaseDate(1999);
+        secondMovie.setDurationMinutes(139);
+        secondMovie.setIncludedInPlan(true);
 
 
-        System.out.println("\n Aqui la segunda pelicula");
+        System.out.println("\nAquí la segunda película");
         secondMovie.ShowSpecs();
         secondMovie.scoreAnalyzer(9.9);
         secondMovie.scoreAnalyzer(9.1);
         secondMovie.scoreAnalyzer(9.6);
         System.out.println(secondMovie.averageScore());
-        System.out.println(secondMovie.scoreCount);
-
-
-
     }
 }
